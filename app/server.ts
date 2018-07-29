b0d89be139cfa4ab42a099359d3fe23bc4f77a96
DucAnhPhi <ducanhphi1@gmail.com>
2018-07-29 16:12:40 +0200

    feat: add initial express app with typescript

---
+++ app/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { WelcomeController } from './controllers';
+
+const app: express.Application = express();
+const port: string = process.env.PORT || '3000';
+
+app.use('/welcome', WelcomeController);
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/`)
+});
---
+++ build/controllers/welcome.controller.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var router = express_1.Router();
+router.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+router.get('/:name', function (req, res) {
+    res.send("Hello, " + req.params.name);
+});
+exports.WelcomeController = router;
