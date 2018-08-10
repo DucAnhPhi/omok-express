21dbb79364af165c2d7c74df03c2461abc2bdae9
DucAnhPhi <ducanhphi1@gmail.com>
2018-08-10 21:44:39 +0200

    feat: check authenticated connection with firebase sdk

--- app/app.ts
+++ app/app.ts
@@ -3,12 +3,32 @@ import http from "http";
 import socketIo from "socket.io";
 import GameListNamespace from "./namespaces/gameList";
 import GameNamespace from "./namespaces/game";
+import * as admin from "firebase-admin";
+
+const firebaseAccountKey = require("../config/firebaseAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseAccountKey),
+  databaseURL: "https://omok-8943b.firebaseio.com"
+});
 
 const app = express();
 const server = new http.Server(app);
 const io = socketIo(server);
 
 server.listen(3000);
+
+io.use((socket: socketIo.Socket, next: (err?: any) => void) => {
+  if (socket.handshake.query && socket.handshake.query.token) {
+    admin
+      .auth()
+      .verifyIdToken(socket.handshake.query.token)
+      .then(() => next())
+      .catch(() => next(new Error("Authentication error")));
+  } else {
+    next(new Error("Authentication error"));
+  }
+});
 
 const gameListNamespace = new GameListNamespace(io);
 const gameNamespace = new GameNamespace(io);
